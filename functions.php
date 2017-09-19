d70c0516a92600bd76308840383ced26bf7de321
Angelos Theodorakopoulos <angtheod@gmail.com>
2017-09-19 12:27:35 +0300

    Updated to 0.2.3. Removed double parent-style enqueue since it is being load from the master-theme functions.php file. Deleted base.twig of child-theme as agreed, so that we can still have the child-theme activate and at the same time it will not override the base.twig of master-theme. NROs that want to use the child-theme will need to create that file.

--- functions.php
+++ functions.php
@@ -4,9 +4,8 @@
  * Additional code for the child theme goes in here.
  */
 
-add_action( 'wp_enqueue_scripts', 'enqueue_parent_styles');
+add_action( 'wp_enqueue_scripts', 'enqueue_child_styles');
 
-function enqueue_parent_styles() {
-   wp_enqueue_style( 'parent-style', get_template_directory_uri() . '/style.css' );
-	 wp_enqueue_style( 'child-style', get_stylesheet_directory_uri() . '/style.css' );
+function enqueue_child_styles() {
+	wp_enqueue_style( 'child-style', get_stylesheet_directory_uri() . '/style.css' );
 }
