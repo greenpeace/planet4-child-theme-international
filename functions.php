eee2ddb8d840dbac08357ed4cb0520e31a41f4e3
Nikos Roussos <nikos@roussos.cc>
2018-02-26 13:44:15 +0200

    Load css after parent template
    * Add versioning

--- functions.php
+++ functions.php
@@ -4,8 +4,10 @@
  * Additional code for the child theme goes in here.
  */
 
-add_action( 'wp_enqueue_scripts', 'enqueue_child_styles');
+add_action( 'wp_enqueue_scripts', 'enqueue_child_styles', 99);
 
 function enqueue_child_styles() {
-	wp_enqueue_style( 'child-style', get_stylesheet_directory_uri() . '/style.css' );
+	$css_creation = filectime(get_stylesheet_directory() . '/style.css');
+
+	wp_enqueue_style( 'child-style', get_stylesheet_directory_uri() . '/style.css', [], $css_creation );
 }
