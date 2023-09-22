1df738ebe2a6316a330e7d8bf7b4727f90efd0c4
Nikos Roussos <nikos.roussos@greenpeace.org>
2023-09-22 10:01:10 +0300

    Remove Country Selector AB testing code

--- functions.php
+++ functions.php
@@ -12,14 +12,6 @@ function enqueue_child_styles() {
 	wp_enqueue_style( 'child-style', get_stylesheet_directory_uri() . '/style.css', [], $css_creation );
 }
 
-add_action( 'wp_enqueue_scripts', 'enqueue_child_scripts',  100);
-
-function enqueue_child_scripts() {
-	$js_creation = filectime(get_stylesheet_directory() . '/src/country-selector-banner.js');
-
-	wp_enqueue_script( 'country-selector-banner', get_stylesheet_directory_uri() . '/src/country-selector-banner.js', [], $js_creation, true );
-}
-
 add_filter( 'locale', function() {
 	return 'en';
 });
